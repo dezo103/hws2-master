310d0e8e692dac4c8eec5b1b92e09429082f0aa6
dezo103 <dezo103@gmail.com>
2023-10-10 21:46:56 +0300

    fix: add burger menu from homework 5

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import s from './App.module.css'
 import HW1 from '../s2-homeworks/hw01/HW1'
 import HW13 from "../s2-homeworks/hw13/HW13";
+import HW5 from "../s2-homeworks/hw05/HW5";
 
 function App() {
     return (
@@ -13,6 +14,7 @@ function App() {
             {/*<HW4/>*/}
 
             {/*при выполнении дз 5 и более - закомментировать здесь дз 1-4, так как они есть внутри дз 5*/}
+            <HW5/>
             <HW13/>
         </div>
     )
